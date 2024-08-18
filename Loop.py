d1e9b2085391c24a77ace778959ea9d88042da77
valanarasu <valan_arasu3000@yahoo.com>
2024-08-18 22:47:03 +0530

    Poly and Loop fun

---
+++ Loop.py
@@ -0,0 +1,11 @@
+i = 0
+while i < 7:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+  fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
---
+++ Poly.py
@@ -0,0 +1,29 @@
+class Car:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+  def move(self):
+    print("Drive!" +self.brand + self.model)
+class Boat:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+  def move(self):
+    print("Sail!")
+
+class Plane:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+  def move(self):
+    print("Fly!" + self.brand + self.model)
+
+car1 = Car("Ford", "Mustang")       #Create a Car class
+boat1 = Boat("Ibiza", "Touring 20") #Create a Boat class
+plane1 = Plane("Boeing", "747")     #Create a Plane class
+
+for x in (car1, boat1, plane1):
+  x.move()
